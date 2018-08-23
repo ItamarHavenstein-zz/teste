49af95d379dfddc79b861253b6ed2064f4db7125
Itamar Havenstein <itamarhavenstein@hotmail.com>
2018-08-23 14:17:35 -0300

    algumas mudanças

--- teste/Program.cs
+++ teste/Program.cs
@@ -15,7 +15,13 @@ namespace teste
         static void Main(string[] args)
         {
 
-
+            Console.WriteLine("É preciso fazer todos os algoritmos para aprender");
+            Console.WriteLine("Itamar Havenstein");
+            Console.WriteLine("Fazendo a multiplicação entre dois numeros:");
+            int x = 23;
+            var y = 48;
+            Console.WriteLine("23 multiplica 48: {0}", x * y);
+            Console.ReadKey();
 
         }
     }
